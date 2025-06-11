ed6cfc4b13acc15c26615a52559ddadca8a9ed05
sebastiangr <sebastiangr@proton.me>
2025-06-10 23:17:57 -0500

    Update API initial configuration.

---
+++ packages/api/src/index.ts
@@ -0,0 +1,30 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+// Middlewares
+app.use(cors()); // Enables CORS for all routes
+app.use(express.json()); // Enable JSON parsing for incoming requests
+
+// Test route
+app.get('/api', (req: Request, res: Response) => {
+  res.json({ message: '¡Bienvenido a la API de la Biblioteca Personal!' });
+});
+
+app.get('/', (req: Request, res: Response) => {
+  // res.json({ message: `Servidor corriendo en la raíz PORT: ${PORT}` });
+  res.send(`
+    <h1>Server running on localhost:${PORT}</h1>
+    <p>Go to <a href="/api">/api</a> to test the API.</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀  Server running on http://localhost:${PORT}`);
+});
